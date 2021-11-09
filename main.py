3f7972efbbb343c4cc3a0e31e532aeddf3b509cf
Remerad <jgodgiven@gmail.com>
2021-11-09 21:20:01 +0300

    Убрать мусор, сгладить углы.

--- main.py
+++ main.py
@@ -1,17 +1,42 @@
 import requests, bs4
-#KEYWORDS = ['Python']
-KEYWORDS = ['дизайн', 'фото', 'web', 'python']
-
+import re
+KEYWORDS = ['штраф']
+#KEYWORDS = ['дизайн', 'фото', 'web', 'python']
+for k in KEYWORDS:
+    temp = ''
+    if k[0].isalpha():
+        temp
+    k = f'([{k[0]}{k[0].swapcase()}]{k[1:]}'
+#TODO: переработать в заготовку для регекспа
 KEYWORDS = set(KEYWORDS)
 response = requests.get('https://habr.com/ru/all/')
 response.raise_for_status()
 soup = bs4.BeautifulSoup(response.text, features='html.parser')
+articles_title_url_time = []
+
 articles = soup.find_all('article')
+# article_title_text = articles[0].find(class_='tm-article-snippet__title tm-article-snippet__title_h2').find('span').text
+# article_url_text = 'https://habr.com' + articles[0].find(class_='tm-article-snippet__title tm-article-snippet__title_h2').find('a').get('href')
+# article_datetime_text = articles[0].find(class_='tm-article-snippet__datetime-published').find('time').get('title')
+
+# print(article_title_text)
+# print(article_url_text)
+# print(article_datetime_text)
+# print(articles[0])
+
 for article in articles:
-    hubs = article.find_all(class_='tm-article-snippet__hubs-item')
-    hubs = set(hub.find('span').text for hub in hubs)
-    if KEYWORDS & hubs:
-        href = article.find(class_ = 'tm-article-snippet__title-link').attrs['href']
-        link = 'https://habr.com' + href
-        tm = article.find(class_ = 'tm-article-snippet__datetime-published').text
-        print(tm, '-', article.find('h2').text, '-', link)
+    article_title_text = article.find(class_='tm-article-snippet__title tm-article-snippet__title_h2').find('span').text
+    article_url_text = 'https://habr.com' + article.find(class_='tm-article-snippet__title tm-article-snippet__title_h2').find('a').get('href')
+    article_datetime_text = article.find(class_='tm-article-snippet__datetime-published').find('time').get('title')
+    articles_title_url_time.append({'title': article_title_text, 'url': article_url_text, 'datetime': article_datetime_text})
+
+for article in articles_title_url_time:
+    response = requests.get(article['url'])
+    response.raise_for_status()
+    soup = bs4.BeautifulSoup(response.text, features='html.parser')
+
+    find_all_clothes = soup.find_all(text=re.compile("([Pp]ython)"))
+    if find_all_clothes:
+        print('да')
+    else:
+        print('нет')
